0afc7e3b11b4e4ebb970c793f0e45bbfc2f903e5
miau-murk <moiseenko.km@phystech.edu>
2026-01-15 17:36:01 +0300

    delete dump functions

--- hscc/conf_calc/pyxtb_test.py
+++ hscc/conf_calc/pyxtb_test.py
@@ -3,14 +3,12 @@ from rdkit import Chem
 from .pyxtb_conf_calc import ConfCalc
 from .angles import Conformation
 
-# mol_file = "test0.mol"
 mol_file = "test.mol"
 
 ref_conf = Chem.MolFromMolFile(mol_file, removeHs=False)
 
 all_dih_angles = Conformation.find_unique_dihedral_angles(ref_conf) # ([((1, 2, 3, 4), angle_value)], [ ring ])
 nonring_dih_angles = all_dih_angles[0] # only non-ring angles
-
 rotatable_dih_idx = []
 for dih_angle in nonring_dih_angles:
     rotatable_dih_idx.append(list(dih_angle[0]))
@@ -20,4 +18,4 @@ for dih_angle in nonring_dih_angles:
 calculator = ConfCalc(mol=ref_conf,
                       rotable_dihedral_idxs=rotatable_dih_idx)
 
-print(calculator.get_energy(np.array([1.0]), req_grad=True)) # for sampling
+print(calculator.get_energy(np.random.random(len(nonring_dih_angles)), req_grad=True))
